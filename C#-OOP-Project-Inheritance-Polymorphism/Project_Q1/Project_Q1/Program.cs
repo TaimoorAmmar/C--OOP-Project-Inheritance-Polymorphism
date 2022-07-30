b0e6700c4b31f0bcfa13ded93231fda9541d6112
Taimoor Ammar <101567902+TaimoorAmmar@users.noreply.github.com>
2022-07-30 08:08:54 -0400

    Add files via upload

---
+++ C#-OOP-Project-Inheritance-Polymorphism/Project_Q1/Project_Q1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace Project_Q1
+{
+   
+    public class Vehicle 
+    {
+        private double minimumFee;
+        private double maximumFee;
+        private double excessFee;
+        private string type;
+        public Vehicle(string type,double min, double max,double excessFee) 
+        {
+            this.type = type;
+            this.maximumFee = max;
+            this.minimumFee = min;
+            this.excessFee = excessFee;
+        }
+        public double getMinimumFee()
+        {
+            return this.minimumFee;
+        }
+        public double getMaximumfee()
+        {
+            return this.maximumFee;
+        }
+        public double getExcessFee()
+        {
+            return this.excessFee;
+        }
+        public void setType(string vehicleType)
+        {
+            this.type = vehicleType; 
+        }
+        public string getType()
+        {
+            return this.type;
+        }
+
+    }
+  
+    public class Customer
+    {
+       
+        private Vehicle vehicle;
+        private int time;
+       
+
+        public Customer(Vehicle veh,int duration)
+        {
+            this.vehicle = veh;
+            this.time = duration;
+           
+        }
+
+
+        public double calculateFee()
+        {
+            
+                if (this.time <= 3)
+                {
+                    return vehicle.getMinimumFee();
+                }
+                else
+                {
+                    if (this.time == 24)
+                    {
+                    return vehicle.getMaximumfee();
+                    }
+                    else
+                    {
+                        double excessTime = time - 3;
+                        double total = excessTime * vehicle.getExcessFee();
+
+                    return vehicle.getMinimumFee() + total;
+                    }
+              
+                }
+            
+          
+
+        }
+
+    }
+    
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            Vehicle bike = new Vehicle("bike", 2, 10,0.50);
+            Vehicle bus = new Vehicle("bus", 6, 30,1.5);
+            Vehicle car = new Vehicle("car", 4, 20,1);
+
+            Customer customer1 = new Customer(bike,24);
+            Customer customer2 = new Customer(bike, 1);
+            Customer customer3 = new Customer(bike, 5);
+
+            double Customer1Fee = customer1.calculateFee();
+            double Customer2Fee = customer2.calculateFee();
+            double Customer3Fee = customer3.calculateFee();
+
+
+            Console.WriteLine("\n Bikes \n");
+            Console.WriteLine("Fee for Customer 1 who parked yesterday is $" + Customer1Fee);
+            Console.WriteLine("Fee for Customer 2 who parked an hour ago is $" + Customer2Fee);
+
+            Console.WriteLine("Fee for Customer 3 who parked 5 hours ago is $" + Customer3Fee);
+            //For Cars
+
+            Customer customer1Car = new Customer(car, 24);
+            Customer customer2Car = new Customer(car, 1);
+            Customer customer3Car = new Customer(car, 5);
+
+            double Customer1FeeCar = customer1Car.calculateFee();
+            double Customer2FeeCar = customer2Car.calculateFee();
+            double Customer3FeeCar = customer3Car.calculateFee();
+
+            Console.WriteLine("\n Cars \n");
+
+            Console.WriteLine("Fee for Customer 1 who parked yesterday is $" + Customer1FeeCar);
+            Console.WriteLine("Fee for Customer 2 who parked an hour ago is is $" + Customer2FeeCar);
+
+            Console.WriteLine("Fee for Customer 3 who parked 5 hours ago is is $" + Customer3FeeCar);
+
+            //For Buses
+
+            Customer customer1Bus = new Customer(bus, 24);
+            Customer customer2Bus = new Customer(bus, 1);
+            Customer customer3Bus = new Customer(bus, 5);
+
+            double Customer1FeeBus = customer1Bus.calculateFee();
+            double Customer2FeeBus = customer2Bus.calculateFee();
+            double Customer3FeeBus = customer3Bus.calculateFee();
+
+            Console.WriteLine("\n Buses \n");
+
+            Console.WriteLine("Fee for Customer 1 who parked yesterday is $" + Customer1FeeBus);
+            Console.WriteLine("Fee for Customer 2 who parked an hour ago is is $" + Customer2FeeBus);
+
+            Console.WriteLine("Fee for Customer 3 who parked 5 hours ago is is $" + Customer3FeeBus);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ C#-OOP-Project-Inheritance-Polymorphism/Q2/Q2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Q2
+{
+    public class Employee
+    {
+        private int baseSalary = 1500;
+        private int fuelAllowance;
+        private int medicalAllowance;
+        public virtual int getFuelAllowance()
+        {
+            return this.fuelAllowance;
+        }
+        public virtual int getMedicalAllowance()
+        {
+            return this.medicalAllowance;
+        }
+        public int getBaseSalary()
+        {
+            return this.baseSalary;
+        }
+
+    }
+
+    public class Engineer : Employee
+    {
+        private int fuelAllowance = 100;
+        private int medicalAllowance = 500;
+
+        public override int getFuelAllowance()
+        {
+            return this.fuelAllowance;
+        }
+        public override int getMedicalAllowance()
+        {
+            return this.medicalAllowance;
+        }
+
+    }
+    public class Manager : Employee
+    {
+        private int fuelAllowance = 250;
+        private int medicalAllowance = 1000;
+
+        public override int getFuelAllowance()
+        {
+            return this.fuelAllowance;
+        }
+        public override int getMedicalAllowance()
+        {
+            return this.medicalAllowance;
+        }
+
+    }
+    public class Analyst : Employee
+    {
+        private int fuelAllowance = 150;
+        private int medicalAllowance = 800;
+
+        public override int getFuelAllowance()
+        {
+            return this.fuelAllowance;
+        }
+        public override int getMedicalAllowance()
+        {
+            return this.medicalAllowance;
+        }
+
+    }
+
+    public class SalarySlip
+    {
+        private Employee employee;
+        private string designation;
+
+      
+        public SalarySlip(string designation)
+        {
+            this.designation = designation;
+        }
+
+
+
+        public int getGrossSalary()
+        {
+            if (this.designation == "engineer" )
+            {
+                Engineer engineer = new Engineer();
+
+                this.employee = engineer;
+            }
+            else if (this.designation == "manager")
+            {
+                Manager manager = new Manager();
+
+                this.employee = manager;
+            }
+            else if (this.designation == "analyst")
+            {
+                Analyst analyst = new Analyst();
+
+                this.employee = analyst;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a correct designation");
+            }
+            int baseSalary = employee.getBaseSalary();
+            int fuelAllowance = employee.getFuelAllowance();
+            int medicalAllowance = employee.getMedicalAllowance();
+            int totalGross = baseSalary + fuelAllowance + medicalAllowance;
+            return totalGross;
+        }
+    }
+    class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Enter a designation i.e 'engineer', 'manager' or 'analyst'");
+            string designation = Console.ReadLine();
+            SalarySlip salary = new SalarySlip(designation);
+            
+            Console.WriteLine("Gross Salary is = $"+ salary.getGrossSalary());
+
+        }
+    }
+}
